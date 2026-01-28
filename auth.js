ab34bc00491cf676726b78a6a1e59edaeb2e15a3
arnaudnenquin-afk <arnaud.nenquin@gmail.com>
2026-01-28 21:43:55 +0100

    Refactor auth functions to use getElementById

--- auth.js
+++ auth.js
@@ -1,29 +1,38 @@
 function login() {
   firebase.auth()
-    .signInWithEmailAndPassword(email.value, password.value)
+    .signInWithEmailAndPassword(
+      document.getElementById("email").value,
+      document.getElementById("password").value
+    )
     .then(() => window.location.href = "app.html")
     .catch(e => alert(e.message));
 }
 
 function register() {
   firebase.auth()
-    .createUserWithEmailAndPassword(email.value, password.value)
+    .createUserWithEmailAndPassword(
+      document.getElementById("email").value,
+      document.getElementById("password").value
+    )
     .then(() => window.location.href = "app.html")
     .catch(e => alert(e.message));
 }
 
 function resetPassword() {
-  if (!email.value) {
+  const email = document.getElementById("email").value;
+  if (!email) {
     alert("Entre ton email");
     return;
   }
 
   firebase.auth()
-    .sendPasswordResetEmail(email.value)
-    .then(() => alert("Email envoyé"))
+    .sendPasswordResetEmail(email)
+    .then(() => alert("Email de récupération envoyé"))
     .catch(e => alert(e.message));
 }
 
 function logout() {
-  firebase.auth().signOut();
+  firebase.auth().signOut().then(() => {
+    window.location.href = "index.html";
+  });
 }
